376b721ead284e7a8491adcb7b772b0ae68ab665
CarolinaUjueta <c_ujueta@javeriana.edu.co>
2025-09-24 20:55:12 -0500

    Create modulo.h

---
+++ SO_projects/TallerModular/modulo.h
@@ -0,0 +1,20 @@
+/**************************************************************************************
+ * Fecha: 23 de Septiembre, 2025
+ * Autor: Carolina Ujueta Ricardo
+ * Materia: Sistemas Operativos
+ * Tema: Concurrencia
+ * Objetivo: Hacer una metodología para la implementación de la multiplicación de matrices.
+***************************************************************************************/
+
+/* Definicion del archivo para luego poder implementarlo */
+#ifndef _MODULO_H_
+#define _MODULO_H_
+
+/* Prototipos de las funciones implementadas en modulo.c)*/
+void InicioMuestra();
+void FinMuestra();
+void iniMatriz(int n, double *m1, double *m2);
+void imprMatrices(int n, double *matriz);
+
+/* Fin de la definicion */
+#endif
